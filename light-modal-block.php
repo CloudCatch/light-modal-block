57b878fe4c4813cea258e55e8fb564506f26d0eb
David Jensen <david@dkjensen.com>
2023-06-16 11:04:19 -0400

    fix: update css styles

--- light-modal-block.php
+++ light-modal-block.php
@@ -6,6 +6,7 @@
  * Requires PHP:      7.0
  * Version:           0.0.0-development
  * Author:            CloudCatch LLC
+ * Author URI:        https://cloudcatch.io
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       light-modal-block
